3ca0ebbe91ea24b6741f2299bf9c26afaff3e786
SteveSant26 <bryanmenoscal2005@gmail.com>
2024-09-26 07:47:40 -0500

    m

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = 'django'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "TRUE"
+DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS')
 
 
 # Application definition
